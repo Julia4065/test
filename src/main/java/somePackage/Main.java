83b83afbbe38fc2532c7ce7edde9cb68cdf170cd
jullia4065 <jullia4065@gmail.com>
2021-04-21 20:05:44 +0300

    commit3

--- src/main/java/somePackage/Main.java
+++ src/main/java/somePackage/Main.java
@@ -3,5 +3,6 @@ package somePackage;
 public class Main {
     public static void main(String[] args) {
         System.out.println("First commit");
+        System.out.println("Third commit");
     }
 }
