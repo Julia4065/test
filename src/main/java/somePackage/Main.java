24e7a0cfd2f6fb52e147a6cc201c5c62d019e9ee
jullia4065 <jullia4065@gmail.com>
2021-04-21 18:44:48 +0300

    commit 9

--- src/main/java/somePackage/Main.java
+++ src/main/java/somePackage/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("5 Commit for master");
         System.out.println("6 Commit for newBranch - rebase testing");
         System.out.println("8` Commit for master");
+        System.out.println("9 Commit for master - rebase");
     }
 }
