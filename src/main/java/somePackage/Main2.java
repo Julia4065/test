39c381e6fd95f463e3a0fc0e65d5f2dffd24e733
jullia4065 <jullia4065@gmail.com>
2021-04-21 20:05:07 +0300

    commit2

--- src/main/java/somePackage/Main2.java
+++ src/main/java/somePackage/Main2.java
@@ -2,6 +2,6 @@ package somePackage;
 
 public class Main2 {
     public static void main(String[] args) {
-        System.out.println("testing3");
+        System.out.println("second commit");
     }
 }
