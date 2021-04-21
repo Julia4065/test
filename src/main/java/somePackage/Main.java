2644c4f0e2b832e73e3f3162034fb2d959d04cd8
jullia4065 <jullia4065@gmail.com>
2021-04-21 20:03:30 +0300

    commit 1

--- src/main/java/somePackage/Main.java
+++ src/main/java/somePackage/Main.java
@@ -2,17 +2,6 @@ package somePackage;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Second commit");
-        System.out.println("Commit from newBranch");
-        System.out.println("4 Commit for master");
-        System.out.println("5 Commit for master");
-        System.out.println("6 Commit for newBranch - rebase testing");
-        System.out.println("8` Commit for master");
-        System.out.println("9 Commit for master - rebase");
-        System.out.println("10 Commit for master - rebase");
-        System.out.println("11 Commit for master - rebase");
-        System.out.println("12 Commit for master - rebase");
-        System.out.println("13 Commit for master - rebase");
+        System.out.println("First commit");
     }
 }
