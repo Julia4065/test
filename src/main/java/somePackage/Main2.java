32726d912255920708ce02d5fbf70fa009e9ddf1
jullia4065 <jullia4065@gmail.com>
2021-04-22 11:10:08 +0300

    testBranch2 - 1

--- src/main/java/somePackage/Main2.java
+++ src/main/java/somePackage/Main2.java
@@ -3,5 +3,6 @@ package somePackage;
 public class Main2 {
     public static void main(String[] args) {
         System.out.println("second commit");
+        System.out.println("testBranch2 commit");
     }
 }
